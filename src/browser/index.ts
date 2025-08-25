1321e5ad3b693a879f1dc8adb28aa28219212cdc
Adarsh Hatkar <80952515+AdarshHatkar@users.noreply.github.com>
2025-08-26 02:06:39 +0530

    remove non react

--- src/browser/index.ts
+++
@@ -1,5 +0,0 @@
-// Browser-specific utilities
-// Currently there are no browser-only modules, but this file is prepared for future additions
-
-// Re-export universal utilities for convenience
-export * from '../universal';
